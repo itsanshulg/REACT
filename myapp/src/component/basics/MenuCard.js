0179227897b6ef784d41c9d284f60c0238c04783
anshul goyal <anshulgoyal211@gmail.com>
2021-10-18 00:21:34 +0530

    react restaurant app

--- myapp/src/component/basics/MenuCard.js
+++ myapp/src/component/basics/MenuCard.js
@@ -8,18 +8,18 @@ const MenuCard = ({ menuData }) => {
                 {menuData.map((curElem) => {
                     return (
                         <>
-                            <div className="card-container">
+                            <div className="card-container" key={curElem.id}>
                                 <div className="card"> 
                                     <div className="card-body">
-                                        <span className="card-number card-circle subtle">1</span>
-                                        <span className="card-author subtle">Breakfast</span>
-                                        <h2 className="card-title">Maggi</h2>
+                                        <span className="card-number card-circle subtle">{curElem.id}</span>
+                                        <span className="card-author subtle">{curElem.category}</span>
+                                        <h2 className="card-title">{curElem.name}</h2>
                                         <span className="card-description subtle">
-                                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure excepturi eius perspiciatis dolores, unde quod in deleniti tenetur quam? Repellendus necessitatibus voluptates quia similique cumque a exercitationem excepturi eaque, placeat nostrum beatae veniam debitis corporis.
+                                        {curElem.description}
                                         </span>
                                         <div className="card-read">Read</div>
                                     </div>
-                                    {/* <img src={image} alt="images" className="card-media"/> */}
+                                    <img src={curElem.image} alt="images" className="card-media"/>
                                     <span className="card-tag subtle">Order Now</span>
                                 </div>
                             </div>
--- myapp/src/component/basics/Restaurent.js
+++ myapp/src/component/basics/Restaurent.js
@@ -5,8 +5,24 @@ import MenuCard from './MenuCard';
 
 const Restaurent = () => {
     const[menuData,setMenuData]= React.useState(Menu)
+
+    const filterItem = (category) =>{
+        const updatedList = Menu.filter((curElem)=>{
+            return curElem.category === category;
+        });
+        setMenuData(updatedList);
+    };
     return (
         <>
+            <nav className="navbar">
+                <div className="btn-group">
+                    <button className="btn-group__item" onclick={()=> filterItem("breakfast")}>Breakfast</button>
+                    <button className="btn-group__item">Lunch</button>
+                    <button className="btn-group__item">Evening</button>
+                    <button className="btn-group__item">Dinner</button>
+                    <button className="btn-group__item">All</button>
+                </div>
+            </nav>
            <MenuCard menuData={menuData}/>
         </>
     );
