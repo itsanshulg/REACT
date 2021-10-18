30318c51006b0ce3b6cc7499bdf80295ce57c940
anshul goyal <anshulgoyal211@gmail.com>
2021-10-18 19:15:29 +0530

    lab prog 1

--- myapp2/src/components/Projects.js
+++ myapp2/src/components/Projects.js
@@ -6,7 +6,10 @@ const Project=(props)=>{
         <React.Fragment className="projects">
             <h2>PROJECTS</h2>
             <ul className="projects-list">
-                
+                <li>Andoid App for fitness</li>
+                <li>react</li>
+                <li>mongo</li>
+                <li>Python</li>
             </ul>
         </React.Fragment>
     )
