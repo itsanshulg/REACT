b7f4225da6b35021d73ddf765c985a33325574bb
itsanshulg <anshulgoyal211@gmail.com>
2021-12-13 23:52:21 +0530

    MAD lab8

---
+++ PWA-Login-Manifest/public/sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install',event =>
+ console.log('sw is installed',event));
+
+ self.addEventListener('activate',event =>
+ console.log('sw is activated',event));
+
+ self.addEventListener('fetch',event =>
+ console.log('sw is fetched',event));
